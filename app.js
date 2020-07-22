97f3fede20b3796fac24b5a626a6e72190b548f3
skydreamweb <info@skydream.info>
2020-07-22 13:15:58 +0200

    Start function

--- app.js
+++ app.js
@@ -3,7 +3,22 @@ document.addEventListener('DOMContentLoad', ()=>{
     const scoreDisplay = document.querySelector('span')
     const startBtn = document.querySelector('.start')
 
+    // start game
+    function startGame(){
+        currentSnake.forEach(index => squares[index].classList.remove('snake'))
+        squares[appleIndex].classList.remove('apple');
+        clearInterval(interval)
+        score = 0;
+        // create apple
+        direction = 1;
+        scoreDisplay.innerText = score;
+        intervalTime = 1000;
+        currentSnake = [2,1,0];
+        currentSnake = 0;
+        currentIndex.forEach(index => squares[index].classList.add('snake'))
+        interval = setInterval(moveOutcomes, intervalTime)
 
+    }
 
     const width = 10;
     let currentIndex = 0; // first div
